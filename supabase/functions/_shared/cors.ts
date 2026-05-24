982907c39c31ed98ca5a59c5381c0416733d3b5d
castrix-code <henrybeatsdaniel@gmail.com>
2026-05-24 19:12:48 -0400

    Use environment variable for allowed CORS origin

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'https://if-ts-doesn-t-work-im-gay.vercel.app/',
+  'Access-Control-Allow-Origin': Deno.env.get('ALLOWED_ORIGIN') ?? 'https://if-ts-doesn-t-work-im-gay.vercel.app/',
   'Access-Control-Allow-Headers':
     'authorization, x-client-info, apikey, content-type',
 }
